547241a824604008f5a12d56791dd11ac6f1d1e4
Samuca401 <matheuslealbm@gmail.com>
2024-06-29 10:20:10 -0300

    Correção de Bugs

--- script.js
+++ script.js
@@ -170,7 +170,7 @@ function newBlock() {
         });
         localStorage.setItem(localStorageLineKey, JSON.stringify(values));
         showLineValues();
-        inputTitle.value = inputContent.value = "";
+        inputTitle.value = inputContent.value = ""; 
     }
 }
 
@@ -205,8 +205,8 @@ function showLineValues() {
         boxMeta.appendChild(contentBoxMeta);
         boxMeta.appendChild(buttonRemoveBoxMeta);
 
-        contentBoxMeta.addEventListener("click",function(el){
-            let content = el.target.textContent;
+        boxMeta.addEventListener("click",function(){
+            let content = contentBoxMeta.textContent;
             openBoxMeta(values[iterator]['title'],content);
             btAdd.style.display = "none";
             removeLineItem(values[iterator]['title'])
@@ -215,7 +215,6 @@ function showLineValues() {
         boardLine.appendChild(boxMeta);
 
     }
-    //boardLine.innerHTML += `<div id='boxMeta'><p id="title-boxmeta">${values[i]['title']}</p><div id="content-boxmeta">${values[i]['content']}</div></div> <button onclick='removeLineItem("${values[i]['title']}")'><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-arrow-down-circle" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293z" /></svg>`;
 }
 
 function openBoxMeta(title,content) {
